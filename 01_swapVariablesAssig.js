d4727298c7fbad3c4ac3991f83d7b489e7c42a65
prashantyada <prashantyadav1701@gmail.com>
2024-03-08 23:14:27 +0530

    variable  declare & swapvariables
    variable  declare & swapvariables

---
+++ 01_swapVariablesAssig.js
@@ -0,0 +1,25 @@
+console.log("===== Assignment-1 =====");
+console.log("WAP to swap variable values using third variable");
+var sweety = "Sweety"; // 200
+var cutie = "Cutie"; // 100
+console.log("======== Before Swap ==========");
+console.log("Sweety:", sweety, " Cutie:", cutie);
+
+console.log("======== After Swap ==========");
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log("Sweety:", sweety, " Cutie:", cutie);
+
+console.log("===== Assignment-2 =====");
+var n1 = 100; // 200
+var n2 = 200; // 100
+var n3 = 300; //300
+console.log("======== Before Swap ==========");
+console.log("n1:", n1, " n2:", n2, "n3:",n3);
+
+console.log("======== After Swap ==========");
+n1 = n3;
+n2=n1-n2;
+n3=n1-n2;
+ console.log("n1:", n1, " n2:", n2,"n3:", n3);
---
+++ 01_variableAssign.js
@@ -0,0 +1,39 @@
+console.log("===== Assignment-1 =====");
+var Name;
+ Name="Prashant Yadav";
+ console.log(Name);
+
+ var fullName;
+ fullName="Prashant Prakash Yadav";
+console.log(fullName);
+
+
+ console.log("====== Assignment-2 =====");
+ var myLove;
+ myLove="I Love Only JS";
+ console.log(myLove);
+
+ console.log("===== Assignment-3 =====");
+ var firstName;
+ firstName="Prashant";
+ console.log(firstName);
+ var lastName;
+ lastName="Yadav";
+ console.log(lastName);
+ var age;
+ age="23";
+ console.log(age);
+ var address;
+ address="Sangola";
+ console.log(address);
+ var pinCode;
+ pinCode="413308";
+ console.log(pinCode);
+ var gender;
+ gender="Male";
+ console.log(gender);
+ var isMarried;
+ isMarried="No";
+ console.log(isMarried);
+ 
+
